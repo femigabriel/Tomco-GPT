6fe0138450959bf4160c80c2f70e7b725d4627cc
Femi Gabriel <timfemi@gmail.com>
2023-12-25 12:45:39 +0100

    login input

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -5,47 +5,73 @@ import React, { useState } from "react";
 // import Input from '../global/Input'
 
 export const Login = () => {
-  const [firstAns, setfirstAns] = useState("");
-  const [secondAns, setsecondAns] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    if (!firstAns || firstAns.toLowerCase() !== "empathy") {
+    if (!password || !email) {
       setError(true);
       return;
     }
-    if (!secondAns || secondAns.toLowerCase() !== "empathy") {
-      setError(true);
-      return;
-    }
-    // onNextClick();
+    e.preventDefault();
   };
 
   return (
     <div className="flex justify-center items-center pt-20">
       <div className="sha shadow-md w-[650px] h-[650px] px-10 py-7 bg-white rounded-md">
         <div className="text-center text-[1.5em] text-blue-400">Sign In</div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col items-start gap-y-3">
             <label
               htmlFor="name"
               className="text-sm font-medium cursor-pointer"
             >
-              Name
+              Email
             </label>
             <Input
-              type="text"
               className="w-full p-4 bg-transparent border border-gray-200 rounded-lg outline-none"
-              placeholder="Enter your name"
+              placeholder="e.g johndoe@lorem.com"
+              onChange={(e) => setEmail(e.target.value)}
+              pattern="^\S+@\S+\.\S+$"
+              type="email"
             />
+            {error && phone.length <= 0 ? (
+              <label className="text-red-400 font-semibold">
+                This field is required
+              </label>
+            ) : (
+              ""
+            )}
           </div>
-          <Input
-            placeholder="Email"
-            type="text"
-            className="input-field bg-inherit px-5"
-            // onChange={(e) => setfirstAns(e.target.value)}
-          />
+          <div className="flex flex-col items-start gap-y-3">
+            <label
+              htmlFor="name"
+              className="text-sm font-medium cursor-pointer"
+            >
+              Password
+            </label>
+            <Input
+              className="w-full p-4 bg-transparent border border-gray-200 rounded-lg outline-none"
+              placeholder="e.g johndoe@lorem.com"
+              onChange={(e) => setEmail(e.target.value)}
+              pattern="^\S+@\S+\.\S+$"
+              type="email"
+            />
+            {error && password.length <= 0 ? (
+              <label className="text-red-400 font-semibold">
+                This field is required
+              </label>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="pt-20">
+            <button className=" bg-blue-950 text-white float-right p-2 px-5 rounded-md">
+              Next Step
+            </button>
+          </div>
         </form>
       </div>
     </div>
