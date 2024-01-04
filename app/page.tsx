09ba9e599e9b454eb0f6c526139f83b8090e3aba
Femi Gabriel <timfemi@gmail.com>
2024-01-04 23:23:45 +0100

    login page to dashboard fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import { Layout } from "@/components/auth/Layout";
-import { Login } from "@/components/auth/login/Login";
+
 import { Index } from "@/components/dashboard/Index";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    // <Layout>
-    //   {/* <Login /> */}
-    //   <Index />
-    // </Layout>
     <div>
-        <Index />
+      <Index />
     </div>
   );
 }
